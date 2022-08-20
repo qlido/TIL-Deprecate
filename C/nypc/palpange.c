9996d8f5c889a7e1c86d29e4646522e2af7f133f
qlido <67864410+qlido@users.noreply.github.com>
2022-08-21 00:18:36 +0900

    ::update 2022-08-20

--- C/nypc/palpange.c
+++ C/nypc/palpange.c
@@ -1,26 +1,49 @@
 #include <stdio.h>
+#include <math.h>
 
-int i,j,x,y,n,m;
-int arr[100][100]={1};
-int dy[4]={0,1,0,-1},dx[4]={1,0,-1,0};
+int i, x, y, n;
+//int arr[100][100]={1};
+int dy[4] = {0, 1, 0, -1}, dx[4] = {1, 0, -1, 0};
+int VX1, VX2, VY1, VY2, RX, RY;
 
-int safe (int a, int b){return (0<=a && a<n)&&(0<=b && b<n);}
+int safe(int a, int b) { return (0 <= a && a < n) && (0 <= b && b < n); }
+
 int main() {
-    int ny, nx, cnt = 1;
-    scanf("%d", &n);
-    int s = n * n;
-    while (s>cnt){
-        ny = y+dy[i%4], nx=x+dx[i%4];
-        if(safe(ny,nx) && !arr[ny][nx]){
-            arr[ny][nx]=++cnt;
-            y=ny,x=nx;
-        } else i++;
+    int ny, nx, cnt = 1, what, s, first, second;
+    scanf("%d", &what);
+    while (what) {
+        int arr[300][300] = {1};
+        scanf("%d %d %d", &n, &first, &second);
+        s = n * n;
+        cnt = 1, y = 0, x = 0, VX2 = 0;
+        VX1 = 0, VY1 = 0;
+        VY2 = 0;
+        while (s > cnt) {
+
+            ny = y + dy[i % 4], nx = x + dx[i % 4];
+            if (safe(ny, nx) && !arr[ny][nx]) {
+                arr[ny][nx] = ++cnt;
+                y = ny, x = nx;
+                if (cnt == first) {
+                    VX1 = x;
+                    VY1 = y;
+                } else if (cnt == second) {
+                    VX2 = x;
+                    VY2 = y;
+                }
+            } else i++;
+        }
+        what--;
+        RX = abs(VX1 - VX2);
+        RY = abs(VY1 - VY2);
+        if (RX == RY) {
+            printf("YES\n");
+        } else {
+            printf("NO\n");
+        }
+
     }
-    for (i = 0 ; i<n;i++) {
-        for (j = 0; j < n; j++)
-            printf("%d ", arr[i][j]);
-        printf("\n");
-    }
+
     return 0;
 }
 
