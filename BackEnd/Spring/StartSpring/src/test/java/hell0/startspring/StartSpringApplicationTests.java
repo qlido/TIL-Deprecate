a29e8ba525be5f22822eac1727f0161178382089
qlido <jason7530@naver.com>
2022-12-03 20:33:50 +0900

    BackEnd 공부

---
+++ BackEnd/Spring/StartSpring/src/test/java/hell0/startspring/StartSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hell0.startspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
